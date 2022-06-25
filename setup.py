75c72e3f78c010792790396d728cb7d3ac2292e2
Alekxos <mckeehan@stanford.edu>
2022-06-25 15:32:56 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup, find_packages
 setup(name='circuitq',
       version='1.2.1',
       description='Automated design of superconducting qubits',
-      url='https://github.com/PhilippAumann/circuitq',
-      author='CircuitQ Team',
-      author_email='philipp.aumann@uibk.ac.at',
+      url='https://github.com/Alekxos/circuitQUAIL',
+      author='Alekxos',
+      author_email='boulton@stanford.edu',
       license='MIT',
       # packages=['circuitq']
       packages=find_packages(),
-      install_requires= ['numpy', 'networkx', 'sympy', 'scipy'],
+      install_requires= ['numpy', 'networkx', 'sympy', 'scipy', 'jax'],
       )
 
 local_package_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
